069ed54a45e89eb9021f979383e50b014fafafce
Ghemdane <cghemdane1@myges.fr>
2023-09-29 12:08:17 +0200

    commit initail

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+  apiKey: "AIzaSyCS60-eCeG6HjxM0S0IO4OzQNmEUVeytso", 
+  authDomain: "esgi-cloud-celia.firebaseapp.com",
+  projectId: "esgi-cloud-celia",
+})
+
+export default app
